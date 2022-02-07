9c36e8938c5d3ffb990accaf39d07f310b66b8f5
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2022-02-07 13:13:26 +0100

    Add files via upload

---
+++ Vahlalla 0.3 - DEMO/Assets/Script C#/cameraControl.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+// Classe cameraControl per il movimento della videocamera
+
+public class cameraControl : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float dist;
+    // Velocità videocamera:
+    [SerializeField] private float velCamera;
+    private float lookAhead;
+
+    
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (dist * player.localScale.x), Time.deltaTime * velCamera);
+    }
+
+
+}
