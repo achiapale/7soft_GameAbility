ef5b639e2d122351f0a881f73a7a9eb96fc7303f
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2022-02-12 15:39:10 +0100

    Update Mushroom.cs

--- Vahlalla 0.3 - DEMO/Assets/Script C#/Mushroom.cs
+++ Vahlalla 0.3 - DEMO/Assets/Script C#/Mushroom.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
-//controllo del fungo
+// controllo del fungo
 
 public class Mushroom : MonoBehaviour
 {
+    // Dichiaro animator e rigidBody
     private Animator anim;
     private Rigidbody2D body;
 
@@ -13,13 +14,14 @@ public class Mushroom : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
     }
 
-    //controllo della collisione
-    //animazione per la morte del fungo
 
+    // controllo della collisione:
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            // animazione per la morte del fungo
             anim.SetTrigger("morto");
 
         }
