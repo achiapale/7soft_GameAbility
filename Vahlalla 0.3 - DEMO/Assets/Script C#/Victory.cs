251d16fc7f666f5b758f4629624cf927ac9bfa0e
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2022-02-12 15:40:13 +0100

    Update Victory.cs

--- Vahlalla 0.3 - DEMO/Assets/Script C#/Victory.cs
+++ Vahlalla 0.3 - DEMO/Assets/Script C#/Victory.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Script per la schermata di vittoria:
+
 public class Victory : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Se il player tocca la chest finale, carico scena 5:
+        
         if (collision.tag == "chest")
         {
             SceneManager.LoadScene(5);
 
         }
     }
-
 
 }
